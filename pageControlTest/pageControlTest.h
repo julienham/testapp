9d46419022bb24cf7181ea7738f020ddbd631cd2
Julien Hamon <julien.hamon@ineat-conseil.fr>
2013-01-11 17:32:25 +0100

    rajout du projet de test

---
+++ pageControlTest/pageControlTest.h
@@ -0,0 +1,13 @@
+//
+//  pageControlTest.h
+//  pageControlTest
+//
+//  Created by Julien Hamon on 11/01/13.
+//
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface pageControlTest : SenTestCase
+
+@end
---
+++ pageControlTest/pageControlTest.m
@@ -0,0 +1,32 @@
+//
+//  pageControlTest.m
+//  pageControlTest
+//
+//  Created by Julien Hamon on 11/01/13.
+//
+//
+
+#import "pageControlTest.h"
+
+@implementation pageControlTest
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in pageControlTest");
+}
+
+@end
